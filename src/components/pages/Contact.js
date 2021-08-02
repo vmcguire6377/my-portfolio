61beeac2d449d828dea68bc13597c2dd2130395d
VeeMcGee <vmcguire6377@outlook.com>
2021-08-01 22:28:40 -0600

    created forms on contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { emailValidation } from '../../utils/helpers';
 
-const Contact = () => (
-  <div>
+function Contacts() {
+const [formsState, setFormsState] = useState({ name: '', email: '', message: '' });
+const [errMsg, setErrMsg] = useState('');
+const { name, email, message } = formsState;
+
+function Update(evt) {
+  if (evt.target.name === 'email') {
+    const isValid = emailValidation(evt.target.value);
+    if (!isValid) {
+      setErrMsg('This is not a valid email address. Please try again.');
+    } else {
+      setErrMsg('');
+    }
+  } else {
+    if (!evt.target.value.length) {
+      setErrMsg(`${evt.target.name} is required!`);
+    } else {
+      setErrMsg('');
+    }
+  }
+  if (!errMsg) {
+    setFormsState({ ...formsState, [evt.target.name]: evt.target.value })
+  }
+}
+function updateSubmit(evt) {
+  evt.preventDefault();
+}
+return (
+
+<section className="container">
     <h1>Contact Page</h1>
-        <section id="contact" class="contact">
-        <h2>Contact</h2>
+    <form className="justify-content-left"
+    id="contact-page">
+      <div className="my-5">
+        <label htmlFor="name">Name: </label>
+        <input className="forms-monitor"
+        type="text" name="name"
+        defaultValue={name} onBlur={Update} />
+      </div>
 
-        <h3>Phone: </h3><p>406.207.7382 </p>
-        <h3>Email: </h3><a href="vmcguire6377@gmail.com">vmcguire6377@gmail.com </a>
-        <h3>LinkedIn: </h3>
-        <a href="https://linkedin.com/in/vanessa-mcguire-70927a1ba">https://linkedin.com/in/vanessa-mcguire-70927a1ba</a>
-      </section>
+      <div className="my-5">
+        <label htmlFor="message">Message: </label>
+        <textarea className="formsMonitor" name="message"
+        defaultValue={message} onBlulr={Update} rows="7" />
+      </div>
+{errMsg && (
+  <div>
+    <p className="err-text">{errMsg}</p>
   </div>
+)}
+
+<div className="my-5 mb-5" >
+  <button className="btn btn-outline-light"
+  type="submit"
+  onSubmit={updateSubmit}>Submit</button>
+</div>
+
+</form>
+
+</section>
+
 );
+}
     
 
-    export default Contact;
+    export default Contacts;
