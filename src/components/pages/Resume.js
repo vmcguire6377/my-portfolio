db54b2615e077ea0bc076b54f3072689300a37aa
Vanessa McGuire <79176079+vmcguire6377@users.noreply.github.com>
2021-08-02 18:36:26 -0600

    Update Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,21 +6,23 @@ const Resume = () => (
 
 
 <section id="resume" className="resume">
-  <h2>Resume
+  <h2>Resume</h2>
     <br></br>
-    <br></br>
-    <h1>Technical Proficiencies: HTML, CSS, GitHub, 
+    
+    <h2>Technical Proficiencies:</h2>
+    <h3> HTML, CSS, GitHub, 
     Responsive Web Design, JavaScript, GraphQL, 
     Ajax, jQuery, Bootstrap, Bulma, Object Oriented
      Programming, Object Relational Mapping, Model 
      View Controller, client and server side APIs, 
      jest, Insomnia, MySQL, Sequelize, Cloudinary, 
      and PWAs.
-     </h1>
-  </h2>
-  <div> Please feel free to view or download my resume via the link below.</div>
+     <br></br>
+     Please feel free to view or download my resume via the link below.
+     </h3>
+  
   <br></br>
-  <a href="https://drive.google.com/file/d/1b4VoZSEQccyV_Aphv_FWDttNJBwldh0e/view?usp=sharing">https://drive.google.com/file/d/1b4VoZSEQccyV_Aphv_FWDttNJBwldh0e/view?usp=sharing</a>
+  <a href="https://drive.google.com/file/d/1b4VoZSEQccyV_Aphv_FWDttNJBwldh0e/view?usp=sharing">Professional Resume</a>
 </section>
 
 );
